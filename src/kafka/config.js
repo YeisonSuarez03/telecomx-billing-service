3a49c909e2a09cf80ddc00da33d0cc89d9cdb4b5
Yeison Suarez <yeisong12ayeisondavidsuarezg12@gmail.com>
2025-10-05 20:22:37 -0500

    fix: Kafka changes

--- src/kafka/config.js
+++ src/kafka/config.js
@@ -8,7 +8,7 @@ class KafkaConfig {
     const brokers = (process.env.KAFKA_BROKERS || 'localhost:9092').split(',');
     const clientId = process.env.KAFKA_CLIENT_ID || 'telecomx-billing-service';
 
-    this.kafka = new Kafka({ clientId, brokers });
+    this.kafka = new Kafka({ clientId, brokers: [...brokers] });
     KafkaConfig.instance = this;
   }
 
