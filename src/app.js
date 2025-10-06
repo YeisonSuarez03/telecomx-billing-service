9af2e879dc53218b054b26debd7a466880b8380c
Yeison Suarez <yeisong12ayeisondavidsuarezg12@gmail.com>
2025-10-05 23:52:25 -0500

    fix: comment cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 
@@ -6,7 +5,7 @@ import billingsRouter from './routes/billings.js';
 
 const app = express();
 app.use(morgan('dev'));
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.use('/billings', billingsRouter);
